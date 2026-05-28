f8bc1a58d5615b015ea02e482cb9695c927972ce
theanishtar <long16730z@gmail.com>
2026-05-28 22:58:53 +0700

    ...

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -40,7 +40,7 @@
                 </svg>
                 <span>Căn hộ</span>
             </a>
-            <a href="{{ route('admin.invitations.create') }}" class="dashboard-nav__item {{ request()->routeIs('admin.invitations.*') ? 'dashboard-nav__item--active' : '' }}">
+            <a href="{{ route('admin.invitations.index') }}" class="dashboard-nav__item {{ request()->routeIs('admin.invitations.*') ? 'dashboard-nav__item--active' : '' }}">
                 <svg class="dashboard-nav__icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <circle cx="12" cy="12" r="10"></circle>
                     <line x1="12" y1="8" x2="12" y2="16"></line>
