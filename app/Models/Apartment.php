e382b98b97a0e8a4656ca2e3bac2b6d2f49f10ef
Do Viet Hung <doviethung1245@gmail.com>
2026-07-22 18:31:11 +0700

    sua loi

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -81,6 +81,7 @@ class Apartment extends Model
      * Users thuộc căn hộ này
      */
     public function users(): HasMany
+    {
         return $this->hasMany(User::class);
     }
 
@@ -117,6 +118,8 @@ class Apartment extends Model
     /**
      * Scope căn hoạt động
      */
+    public function scopeActive($query)
+    {
         return $query->where('status', '!=', 'maintenance');
     }
 
