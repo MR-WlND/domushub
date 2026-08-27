91219f64104223385074cdc0d6ed03ca1e90d9d0
Do Viet Hung <doviethung1245@gmail.com>
2026-08-27 14:56:25 +0700

    sau tam tru

--- resources/views/receptionist/temporary-registrations/index.blade.php
+++ resources/views/receptionist/temporary-registrations/index.blade.php
@@ -599,7 +599,9 @@
                                         @endif
                                         {{-- Nút Gia hạn --}}
                                         <button type="button" class="action-btn" title="Gia hạn" onclick="openExtendModal({{ $reg->id }}, '{{ $reg->end_date ? $reg->end_date->format('Y-m-d') : '' }}')">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                                            </svg>
                                         </button>
 
                                         {{-- Nút Kết thúc sớm --}}
